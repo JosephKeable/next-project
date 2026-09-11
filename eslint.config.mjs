6cef4d5945a354757271216244aa80a3d4fbb373
Joseph Keable <keablejoseph@gmail.com>
2026-09-11 11:30:41 +0100

    fixes eslint bs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,25 +3,25 @@ import nextVitals from 'eslint-config-next/core-web-vitals'
 import stylistic from '@stylistic/eslint-plugin'
 
 const eslintConfig = defineConfig([
-    ...nextVitals,
-    // Override default ignores of eslint-config-next.
-    globalIgnores([
-        // Default ignores of eslint-config-next:
-        '.next/**',
-        'out/**',
-        'build/**',
-        'next-env.d.ts',
-    ]),
-    {
-        plugins: {
-            '@stylistic': stylistic
-        },
-        rules: {
-            'indent': ['error', 2],
-            '@stylistic/indent': ['error', 2],
-            // ...
-        },
-    }
+  ...nextVitals,
+  // Override default ignores of eslint-config-next.
+  globalIgnores([
+    // Default ignores of eslint-config-next:
+    '.next/**',
+    'out/**',
+    'build/**',
+    'next-env.d.ts',
+  ]),
+  {
+    plugins: {
+      '@stylistic': stylistic
+    },
+    rules: {
+      'indent': ['error', 2],
+      '@stylistic/indent': ['error', 2],
+      // ...
+    },
+  }
 
 ])
 
